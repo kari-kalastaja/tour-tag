4394bbdc8dae6c032306e848ab744a80f0445942
Santeri <santeri.qvintus@aalto.fi>
2021-03-20 13:06:14 +0200

    Ulkosivun alotus (css ei toimi)

--- tour_tag_project/tour_tag_project/settings.py
+++ tour_tag_project/tour_tag_project/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for tour_tag_project project.
 
@@ -54,7 +56,8 @@ ROOT_URLCONF = 'tour_tag_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        #'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
