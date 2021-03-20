c39a03bea3d1057b87b90b8461b1d5c8706a5088
Santeri <santeri.qvintus@aalto.fi>
2021-03-20 22:55:49 +0200

    Admin page linkitys toimii

--- tour_tag_project/tour_tag_project/urls.py
+++ tour_tag_project/tour_tag_project/urls.py
@@ -19,5 +19,6 @@ from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
 ]
