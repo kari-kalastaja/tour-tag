c00da025487682214499b1c5e8c3c785e2c4b3d5
kari kala <topi_kahila@hotmail.com>
2021-03-22 11:10:33 +0200

    destination page upgrade

--- tour_tag_project/users/models.py
+++ tour_tag_project/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    destination = models.CharField(max_length=100)
+    arrive_time = models.CharField(max_length=100)
--- tour_tag_project/users/views.py
+++ tour_tag_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from .models import Destination
 
 # Create your views here.
 def home(request):
@@ -12,5 +13,26 @@ def logout(request):
     logout(request)
     return redirect('home')
 
+#def addDestination(request):
+#    return render(request, 'addDestination.html')
+
 def addDestination(request):
-    return render(request, 'addDestination.html')
+    #return render(request, 'addDestination.html')
+    print('in add destination')
+
+    if request.method == 'POST':
+
+        print("ifiss")
+        destination = request.POST['destination']
+        arrive_time = request.POST['arrive_time']
+
+        des = Destination(destination = destination, arrive_time = arrive_time)
+        des.save()
+        print('destination created')
+
+        return render(request, 'addDestination.html')
+
+        
+    else:
+        print("elses")
+        return render(request, 'addDestination.html')
