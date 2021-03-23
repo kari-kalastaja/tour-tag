ca8b7a2d4ce1c0fb33044bd7edc5eb8eca31114c
Santtu <santeriq93@gmail.com>
2021-03-23 19:28:59 +0200

    Update settings.py

--- tour_tag_project/tour_tag_project/settings.py
+++ tour_tag_project/tour_tag_project/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# config/settings.py
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
